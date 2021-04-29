e9f02244049be3aa5d878a19fd91d16dc34a2f7f
Ash515 <aaashwin515@gmail.com>
2021-04-29 12:30:09 +0530

    updated

---
+++ Display_Possibilities/display.js
@@ -0,0 +1,15 @@
+function innerHTMLfunction(){
+    document.getElementById('demo').innerHTML="C++";
+}
+function writefunction(){
+    document.write(6+7);
+}
+function alertfunction(){
+    window.alert("Hi JavaScript !!!");
+}
+function consolefunction(){
+    console.log("Hi JavaScript")
+}
+function printfunction(){
+    window.print();
+}
---
+++ External JS/External.js
@@ -0,0 +1,3 @@
+function myfunction(){
+    document.getElementById('demo').innerHTML="JavaScript";
+}
